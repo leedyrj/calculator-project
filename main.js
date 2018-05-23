ae5fa36a8e530a409aed780d6b8b83e9dd147b87
Robert Leedy <leedyrj@gmail.com>
2018-05-23 15:04:42 -0500

    All buttons working.  Needs to be made DRY/defensive

---
+++ main.js
@@ -0,0 +1,38 @@
+// Requirements:
+
+// 1. Create an HTML file that has two input fields to accept the two numbers to perform operations on.
+// 2. Create 4 buttons to perform each of the basic mathematical functions (add, subtract, multiply, divide).
+// 3. In your JavaScript, put an event listener on each of the buttons.
+// 4. When the user performs one of the operations, output the result to another DOM element of your choice.
+
+
+let additionButton = document.getElementById('addition');
+let subtractionButton = document.getElementById('subtraction');
+let multiplicationButton = document.getElementById('multiplication');
+let divisionButton = document.getElementById('division');
+
+additionButton.addEventListener('click', function() {
+    let num1 = document.getElementById('number-one').value;
+    let num2 = document.getElementById('number-two').value;
+    console.log(parseInt(num1) + parseInt(num2))
+});
+
+subtractionButton.addEventListener('click', function() {
+    let num1 = document.getElementById('number-one').value;
+    let num2 = document.getElementById('number-two').value;
+    console.log(parseInt(num1) - parseInt(num2))
+});
+
+multiplicationButton.addEventListener('click', function() {
+    let num1 = document.getElementById('number-one').value;
+    let num2 = document.getElementById('number-two').value;
+    console.log(parseInt(num1) * parseInt(num2))
+});
+
+divisionButton.addEventListener('click', function() {
+    let num1 = document.getElementById('number-one').value;
+    let num2 = document.getElementById('number-two').value;
+    console.log(parseInt(num1) / parseInt(num2))
+});
+
+
